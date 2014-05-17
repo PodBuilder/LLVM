834485c7cccd28cfcaa09df03fb1ec7c2136d736
William Kent <wjk011@gmail.com>
2014-05-17 16:08:02 -0400

    Add LLVM target

---
+++ LLVM/LLVM.h
@@ -0,0 +1,13 @@
+//
+//  LLVM.h
+//  LLVM
+//
+//  Created by William Kent on 5/17/14.
+//  Copyright (c) 2014 William Kent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLVM : NSObject
+
+@end
