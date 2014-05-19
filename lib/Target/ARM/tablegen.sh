925ebf3cfe2bad69ff311c6e0ce633f3be1b2313
William Kent <wjk011@gmail.com>
2014-05-19 18:48:02 -0400

    Add ARMCommonTableGen target

---
+++ lib/Target/ARM/tablegen.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#  tablegen.sh
+#  LLVM
+#
+#  Created by William Kent on 5/18/14.
+#  Copyright (c) 2014 William Kent. All rights reserved.
+
+set -e
+
+LLVM_TBLGEN=${BUILT_PRODUCTS_DIR}/llvm-tblgen
+[ -x "${LLVM_TBLGEN}" ] || {
+    echo llvm-tblgen missing or not executable 2>&1
+    exit 1
+}
+
+TableGen () {
+    # usage: $0 input.td output.h "-flags -flags -flags"
+    mkdir -p `dirname $2`
+    
+    if [ "$1" -nt "$2" ]; then
+        echo Generating `basename $2`
+        $LLVM_TBLGEN $3 -I ${SRCROOT}/include -o $2 $1
+    fi
+}
+
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenRegisterInfo.inc" "-gen-register-info -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenInstrInfo.inc" "-gen-instr-info -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenCodeEmitter.inc" "-gen-emitter -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenMCCodeEmitter.inc" "-gen-emitter -mc-emitter -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenMCPseudoLowering.inc" "-gen-pseudo-lowering -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenDAGISel.inc" "-gen-dag-isel -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenFastISel.inc" "-gen-fast-isel -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenCallingConv.inc" "-gen-callingconv -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenSubtargetInfo.inc" "-gen-subtarget -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenDisassemblerTables.inc" "-gen-disassembler -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenAsmWriter.inc" "-gen-asm-writer -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenAsmMatcher.inc" "-gen-asm-matcher -I${SRCROOT}/lib/Target/ARM"
