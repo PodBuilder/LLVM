9145cab48a966e77598696335b29f86780a9ccd0
William Kent <wjk011@gmail.com>
2014-05-23 17:31:59 -0400

    Add ARM.xcodeproj

--- lib/Target/ARM/tablegen.sh
+++ lib/Target/ARM/tablegen.sh
@@ -20,19 +20,19 @@ TableGen () {
     
     if [ "$1" -nt "$2" ]; then
         echo Generating `basename $2`
-        $LLVM_TBLGEN $3 -I ${SRCROOT}/include -o $2 $1
+        $LLVM_TBLGEN $3 -I ${TOP_SRCROOT}/include -o $2 $1
     fi
 }
 
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenRegisterInfo.inc" "-gen-register-info -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenInstrInfo.inc" "-gen-instr-info -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenCodeEmitter.inc" "-gen-emitter -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenMCCodeEmitter.inc" "-gen-emitter -mc-emitter -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenMCPseudoLowering.inc" "-gen-pseudo-lowering -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenDAGISel.inc" "-gen-dag-isel -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenFastISel.inc" "-gen-fast-isel -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenCallingConv.inc" "-gen-callingconv -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenSubtargetInfo.inc" "-gen-subtarget -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenDisassemblerTables.inc" "-gen-disassembler -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenAsmWriter.inc" "-gen-asm-writer -I${SRCROOT}/lib/Target/ARM"
-TableGen "${SRCROOT}/lib/Target/ARM/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenAsmMatcher.inc" "-gen-asm-matcher -I${SRCROOT}/lib/Target/ARM"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenRegisterInfo.inc" "-gen-register-info -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenInstrInfo.inc" "-gen-instr-info -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenCodeEmitter.inc" "-gen-emitter -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenMCCodeEmitter.inc" "-gen-emitter -mc-emitter -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenMCPseudoLowering.inc" "-gen-pseudo-lowering -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenDAGISel.inc" "-gen-dag-isel -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenFastISel.inc" "-gen-fast-isel -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenCallingConv.inc" "-gen-callingconv -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenSubtargetInfo.inc" "-gen-subtarget -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenDisassemblerTables.inc" "-gen-disassembler -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenAsmWriter.inc" "-gen-asm-writer -I${SRCROOT}"
+TableGen "${SRCROOT}/ARM.td" "${PROJECT_TEMP_DIR}/ARMCommonTableGen/ARMGenAsmMatcher.inc" "-gen-asm-matcher -I${SRCROOT}"
