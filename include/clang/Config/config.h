3b254b244fbda25b3b9815e372e8566e227320e7
William Kent <wjk011@gmail.com>
2014-05-26 15:52:56 -0400

    Add libclangDriver
    I also added clang’s config.h and removed the files used to generate it.

---
+++ include/clang/Config/config.h
@@ -0,0 +1,5 @@
+#define BUG_REPORT_URL "https://github.com/PodBuilder/LLVM"
+#define CLANG_RESOURCE_DIR "./Resources"
+#define C_INCLUDE_DIRS "/usr/include:/usr/local/include"
+#define DEFAULT_SYSROOT "/"
+#define GCC_INSTALL_PREFIX "/"
